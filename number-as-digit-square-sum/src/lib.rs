0403dd0f5e51f9f18ecee8700db320c7ecf32e4d
Van Huynh Le <v.h.le@protonmail.com>
2024-01-08 21:40:02 +0100

    Problem: Numbers that are a power of their sum of digits

---
+++ number-as-digit-square-sum/src/lib.rs
@@ -0,0 +1,15 @@
+use num::bigint::BigUint;
+
+
+fn power_sum_dig_term(n: u32) -> BigUint {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    }
+}
