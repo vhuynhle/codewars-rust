5adf3065a4c2bdc103c9023b66fb5ec132ee7c8a
Van Huynh Le <vanhuynh@haleytek.com>
2024-01-12 14:58:40 +0100

    Min

---
+++ min-number/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn find_smallest_int(arr: &[i32]) -> i32 {
+    *arr.iter().min().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Rust test example:
+    #[test]
+    fn sample_tests() {
+        assert_eq!(find_smallest_int(&[34, 15, 88, 2]), 2);
+        assert_eq!(find_smallest_int(&[34, -345, -1, 100]), -345);
+    }
+}
